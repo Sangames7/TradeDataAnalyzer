eaa6d86a024e73eee30a8eec589dc85f254cda0d
Sangames Kumar <sangames1997@gmail.com>
2022-05-26 13:28:09 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+const express = require('express')
+
+const app = express()
+const port = 3000
+const routes = require('./routes')
+
+app.use('/',routes)
+
+app.listen(port, () => {
+  console.log(`Express server started on ${port}`);
+})
