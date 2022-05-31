5a1ad50197d39cb56b91627cebd04db17b7dff61
Sangames Kumar <sangames1997@gmail.com>
2022-05-31 19:58:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const fs = require('fs');
+
+const path = require('path');
+
+const xlsx = require('xlsx');
+
+const routes = express.Router();
+
+async function writeToExcel(arraydata){
+    const wb = xlsx.utils.book_new();
+    const finaldata = [];
+    arraydata.forEach(row=>{ 
+        finaldata.push(row.split('|'));
+    })            
+    const ws = xlsx.utils.aoa_to_sheet(finaldata);
+    xlsx.utils.book_append_sheet(wb, ws, 'users_sheet');
+    const filename = "users.xlsx";
+    const wbOpts = {bookType: 'xlsx', type: 'binary'};
+    xlsx.writeFile(wb, filename, wbOpts) ;    
+    return console.log("success");           
+}
+
+routes.get('/test', (req, res) => {
+    console.log(`Worked ${path.join(__dirname, '..', 'example.dat')}`);
+    res.sendStatus(200);
+})
+  
+routes.get('/convert', async(req, res) => {    
+    try{       
+    console.log(path.join(__dirname, '..', 'example.dat'));
+    fs.readFile(path.join(__dirname, '..', 'example.dat'), {encoding:'utf-8'}, async(err, data)=>{        
+        let arraydata = [];
+        if(err)
+            throw err;
+        arraydata = data.split(/\r?\n/);           
+        if(arraydata){        
+            await writeToExcel(arraydata);
+        }                        
+    })
+    res.status(200).send(`Successfully converted`);   
+    }
+    catch(err){
+        console.error(`Error occurred ${err}`);
+        res.status(504).send(`${err}`);
+    }        
+})
+
+module.exports = routes
